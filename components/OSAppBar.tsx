3155a2684e694a0ce6a0affa3a6e77ef7b379a45
MoonFeather <moonbite233@gmail.com>
2025-01-12 18:21:28 +0800

    change AppBar

--- components/OSAppBar.tsx
+++ components/OSAppBar.tsx
@@ -4,6 +4,7 @@ import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import { Autocomplete, IconButton, TextField, Typography } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
 import QrCodeScannerIcon from "@mui/icons-material/QrCodeScanner";
 
 export default function OSAppBar() {
@@ -13,19 +14,22 @@ export default function OSAppBar() {
         className="flex flex-row justify-between items-center"
         variant="regular"
       >
-        <Typography variant="h6">One Store</Typography>
-        <div className="flex flex-row items-center">
-          <Autocomplete
-            className="w-44 -mt-3.5"
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                label="搜索编号或名称"
-                variant="standard"
-              />
-            )}
-            options={[]}
-          />
+        <Typography variant="h5">One Store</Typography>
+        <div className="flex flex-row items-center gap-0 pr-1">
+          {/*<Autocomplete*/}
+          {/*  className="w-44 -mt-3.5"*/}
+          {/*  renderInput={(params) => (*/}
+          {/*    <TextField*/}
+          {/*      {...params}*/}
+          {/*      label="搜索编号或名称"*/}
+          {/*      variant="standard"*/}
+          {/*    />*/}
+          {/*  )}*/}
+          {/*  options={[]}*/}
+          {/*/>*/}
+          <IconButton aria-label="delete" size="large">
+              <SearchIcon />
+          </IconButton>
           <IconButton aria-label="delete" size="large">
             <QrCodeScannerIcon />
           </IconButton>
